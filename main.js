89c08cf256c4d48ad1b57f747e443860b27ed7c8
alibekN54 <alibekandanuarbek@gmail.com>
2023-01-28 13:25:19 +0600

    28.01

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { PointLight } from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -15,10 +16,17 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
 const torus = new THREE.Mesh( geometry, material)
 
 scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20,20,20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+
+scene.add(pointLight, ambientLight)
 
 function animate() {
   requestAnimationFrame(animate);
