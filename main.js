4e8ce79b1344fcc747a0247b03bdf9cc817eb101
alibekN54 <alibekandanuarbek@gmail.com>
2023-02-03 23:08:55 +0600

    css

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+//         _ _ _          _                    _      
+//        | (_) |        | |                  | |     
+//    __ _| |_| |__   ___| | __   ___ ___   __| | ___ 
+//   / _` | | | '_ \ / _ \ |/ /  / __/ _ \ / _` |/ _ \
+//  | (_| | | | |_) |  __/   <  | (_| (_) | (_| |  __/
+//   \__,_|_|_|_.__/ \___|_|\_\  \___\___/ \__,_|\___|
+                                                   
+                                                    
+
 import * as THREE from "three";
 import { PointLight } from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -22,11 +33,34 @@ const torus = new THREE.Mesh( geometry, material)
 scene.add(torus)
 
 const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(20,20,20)
+pointLight.position.set(5, 5, 5)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
-
 scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.5, 25, 25);
+  const material = new THREE.MeshStandardMaterial( {color: 0xffffff} )
+  const star = new THREE.Mesh( geometry, material )
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceSpecture = new THREE.TextureLoader().load('space.jfif');
+scene.background = spaceSpecture;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -34,6 +68,9 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update()
+
   renderer.render( scene, camera);
 
 }
